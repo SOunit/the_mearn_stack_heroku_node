d8fdbc5ebafa61ce1fcf01497429dffefbcf13ee
SOunit <workbookwork7@gmail.com>
2022-02-06 14:17:29 -0800

    move files

--- backend/env.js
+++
@@ -1,8 +0,0 @@
-const keys = {
-  mongo_user: "the_mern_stack_user",
-  mongo_password: "YuTLjrQaTST7K2bT",
-  mongo_db_name: "mern",
-  jwt_secret_key: "supersecret_dont_share_with_anyone",
-};
-
-module.exports = keys;
